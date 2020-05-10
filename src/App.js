64c213627b9a12ba4355368b4ab87f40f08a33c8
daniel8nuevo <nuevo8vida@gmail.com>
2020-05-10 21:18:40 +0300

    added new branch w test in the state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default class LetterCounter extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this. state = { value: null};
+    this.state = { value: null};
   }
   handleChange = e => {
     this.setState({ value : e.target.value });
@@ -20,6 +20,9 @@ export default class LetterCounter extends React.Component {
           onChange={this.handleChange}
           />
         </label>
+        <p>
+          Letter count:
+        </p>
       </div>
     )
   }
