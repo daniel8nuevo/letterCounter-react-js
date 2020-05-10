194cde1308fe88bf359c7dee29dd2713fc1c7e24
daniel8nuevo <nuevo8vida@gmail.com>
2020-05-10 20:32:52 +0300

    added div element w input value

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
 import React from 'react';
 import './App.css';
 
+export default class LetterCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this. state = { value: null};
+  }
+  handleChange = e => {
+    this.setState({ value : e.target.value });
+  }
+  render(){
+    return(
+      <div className="mainDiv">
+        <label>
+          Write a word:
+          <input type='text'
+          value={this.state.value}
+          onChange={this.handleChange}
+          />
+        </label>
+      </div>
+    )
+  }
+}
+
 
